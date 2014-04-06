7a6da516267c522611ba8419ebfe52d8523b6a58
leadmarissa <emailmarissawu@gmail.com>
2014-04-06 03:06:44 -0400

    moredatabaseconfiguration

--- complete.php
+++ complete.php
@@ -20,19 +20,22 @@
 	// PROCEDURAL LOGIC
 	if($email != 0){
 		if(filter_var($email, FILTER_VALIDATE_EMAIL)){ // If email is valid
-			$dup = mysqli_query($con, "SELECT * FROM `signup` WHERE `Email` LIKE '".$email."'");
+			$dup = mysqli_query($con, "SELECT * FROM `signups` WHERE `Email` LIKE '".$email."'");
 			if(mysqli_num_rows($dup) > 0){ // Duplicate
 				$inputEmail = true; // The user tried to input an email
 				$duplicateEmail = true; // Unfortunately, he/she put in a duplicate one
+				echo "Duplicate email" . mysqli_connect_error();
 			}
 			else{ // No duplicates
-				mysqli_query($con, "INSERT INTO signup (Email, Version) VALUES ('".$email."', 0)"); // Add entry into database
+				mysqli_query($con, "INSERT INTO signups (Email, Version) VALUES ('".$email."', 0)"); // Add entry into database
 				$inputEmail = true; // The user entered an email
 				$validEmail = true; // and the user did it successfully!
+				echo "Thanks.  You're already an all-star to us." . mysqli_connect_error();
 			}
 		}
 		else{ // If email is invalid
 			$inputEmail = true; // The user tried to input an email, but it wasn't valid
+			echo "Invalid email." . mysqli_connect_error();
 		}
 	}
 	
